8103b340e04c7a73121060a0d95075f07ca25fd8
Graeme Paris <gparis1986@gmail.comm>
2024-01-22 14:24:56 +0000

    HTML file created and added for Family

--- js2.js
+++ js2.js
@@ -12,20 +12,112 @@ var age2 = 30;
 
 // using && for defensive checks
 
-var me = {
-  name: "Graeme",
-  age: 37,
-  isHappy: true,
-  favCols: ["red", "blue", "white"],
-  //location: { postCode: "KA3 6EE", city: "Kilmarnock" },
-};
+// var family = {
+//   name: "Graeme",
+//   age: 37,
+//   isHappy: true,
+//   favCols: ["red", "blue", "white"],
+//   location: { postCode: "KA3 6EE", city: "Kilmarnock" },
+// }
+// {
+//   name: "Zoe",
+//   age: 35,
+//   isHappy: true,
+//   favCols: ["red", "blue", "white"],
+//   location: { postCode: "KA3 6EE", city: "Kilmarnock" },
+// };
 
-//console.log(me.location);
-console.log(me && me.location && me.location.postCode); // && acts as defensive check that the object exists and then checks the property exists. Even if the property does not exist, it still returns without the error. i.e.
+// var html = "";
+// for ( var i = 0; i < family.length; i++ ) {
+// html = html + "<p>"+ family[i].name "</p>";
+// }
 
-// Ternary operator - allows yo uto resolve with a response other than true or false. Represented in sigle ? 
+// console.log(html);
 
-var age = 12;
-var text = age > 18 ? "You are able to drink" : "You are not able to drink"; // this allows you to validate the age and return message in text format based on if true or false
+//console.log(me.location);var me = {};
+//me.location = { postCode: "KA3 6EE", city: "Kilmarnock" };location: { postCode: "KA3 6EE", city: "Kilmarnock" };
+//console.log(me && me.location && me.location.postCode); // && acts as defensive check that the object exists and then checks the property exists. Even if the property does not exist, it still returns without the error. i.e.
 
-console.log(text);
+// Ternary operator - allows yo uto resolve with a response other than true or false. Represented in sigle ?
+
+//var age = 12;
+//var text = age > 18 ? "You are able to drink" : "You are not able to drink"; // this allows you to validate the age and return message in text format based on if true or false
+
+//console.log(text);
+
+// Control flow - has either if statement or switch statement.
+
+//var age = 29;
+
+//if (age > 18) {
+//  console.log("You are able to drink");
+//} else {
+//  console.log("you are not able to drink");
+// }
+
+//if (age < 18) {
+//  console.log("you are a child"); // validates the < 18 age range
+//} else if (age < 42) {
+//  console.log("you are an adult"); // validates 18-42
+//} else {
+//  console.log("you are a elderly"); // validates 42+
+// }
+
+// switch statement - for concrete cases // type switch and it prepopulates the code, you just need to add more case where needed as well as adding break statements. Break statements ends the block
+
+// switch (age) {
+//   case 17:
+//     console.log("you cannot drink");
+//     break;
+
+//   case 18:
+//     console.log("you can drink");
+//     break;
+
+//   case 19:
+//     console.log("you are steaming");
+//     break;
+
+//   default:
+//     console.log("unable to determine your drunkness"); // works as default return when above conditions arent me
+//     break;
+// }
+
+//loops
+
+// var favCols = ["red", "blue", "white"];
+// // Always need starting point i=0, counter - favCols.length - check all the items. i++ tells to increment the counter by 1.
+// for (var i = 0; i < favCols.length; i++) {
+//   console.log(i, favCols[i]);
+// }
+
+var family = [
+  {
+    name: "Graeme",
+    age: 37,
+    isHappy: true,
+    favCols: ["red", "blue", "white"],
+    location: { postCode: "KA3 6EE", city: "Kilmarnock" },
+  },
+  {
+    name: "Zoe",
+    age: 35,
+    isHappy: true,
+    favCols: ["red", "blue", "white"],
+    location: { postCode: "KA3 6EE", city: "Kilmarnock" },
+  },
+];
+
+var html = "";
+
+for (var i = 0; i < family.length; i++) {
+  html =
+    html +
+    "<div><p>Name:" +
+    family[i].name +
+    "</p><p>Age:" +
+    family[i].age +
+    "</p></div>";
+}
+
+console.log(html);
